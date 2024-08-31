5b7756c326e02348fc02e524463c881f63a6234d
IvanDamNation <ivan.suvalkin@gmail.com>
2024-08-31 15:50:36 +0300

    Add func: change profile. Email required

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import BBLoginView, BBLogoutView, index, profile, other_page
+from .views import (BBLoginView, BBLogoutView, ChangeUserInfoView,
+                    index, profile, other_page)
 
 app_name = 'main'
 urlpatterns = [
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', BBLoginView.as_view(), name='login'),
     path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
--- main/views.py
+++ main/views.py
@@ -1,14 +1,41 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
+from django.urls import reverse_lazy
+from django.views.generic.edit import UpdateView
+
+from .forms import ChangeUserInfoForm
+from .models import AdvUser
 
 
 class BBLoginView(LoginView):
     template_name = 'main/login.html'
+
+
+class BBLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'main/logout.html'
+
+
+class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+    model = AdvUser
+    template_name = 'main/change_user_info.html'
+    form_class = ChangeUserInfoForm
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Данные пользователя изменены'
+
+    def setup(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().setup(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
 
 
 def index(request):
@@ -26,7 +53,3 @@ def other_page(request, page):
     except TemplateDoesNotExist:
         raise Http404
     return HttpResponse(template.render(request=request))
-
-
-class BBLogoutView(LoginRequiredMixin, LogoutView):
-    template_name = 'main/logout.html'
