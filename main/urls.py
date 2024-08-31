6ee8edaa18c4ca8731cc3f135b7b304d038c4917
IvanDamNation <ivan.suvalkin@gmail.com>
2024-08-31 14:00:31 +0300

    Add profile page

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import BBLoginView, index, other_page
+from .views import BBLoginView, index, profile, other_page
 
 app_name = 'main'
 urlpatterns = [
+    path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', BBLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
@@ -11,6 +12,11 @@ class BBLoginView(LoginView):
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+@login_required
+def profile(request):
+    return render(request, 'main/profile.html')
 
 
 def other_page(request, page):
